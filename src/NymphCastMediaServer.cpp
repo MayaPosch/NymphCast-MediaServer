94bccd81b5f3e62c8d67a80d94bf577ba0af8be2
Maya Posch <maya@nyanko.ws>
2024-02-06 19:16:58 +0100

    Add initial file sync code & stubs.

--- src/NymphCastMediaServer.cpp
+++ src/NymphCastMediaServer.cpp
@@ -27,6 +27,9 @@
 
 #include <Poco/Condition.h>
 #include <Poco/Thread.h>
+#include <Poco/StringTokenizer.h>
+#include <Poco/DirectoryWatcher.h>
+#include <Poco/Delegate.h>
 using namespace Poco;
 
 #include <map>
@@ -50,6 +53,27 @@ struct MediaFile {
 };
 
 
+struct Game {
+	std::string name;
+};
+
+
+struct Save {
+	std::string name;
+};
+
+
+struct GameSystem {
+	std::string name;		// Short name is also folder name.
+	std::string long_name;
+	std::string extensions;
+	std::string launch_cmd;
+	std::string theme;
+	std::vector<Game> games;
+	std::vector<Save> saves;
+};
+
+
 struct RemoteServerStatus {
 	NymphPlaybackStatus status;
 	bool init = false;	// True after first connection update.
@@ -63,12 +87,14 @@ struct RemoteServerStatus {
 Condition gCon;
 Mutex gMutex;
 std::vector<MediaFile> mediaFiles;
+std::vector<GameSystem> gameSystems;
 static NymphCastClient client;
 uint32_t handle = 0;
 std::string serverip;
 std::map<uint32_t, bool> receiverStatus;
 std::map<uint32_t, RemoteServerStatus> remoteStatus;
 std::mutex remoteMutex;
+std::vector<Poco::DirectoryWatcher*> dirwatchers;
 // ---
 
 
@@ -274,6 +300,47 @@ NymphMessage* playMedia(int session, NymphMessage* msg, void* data) {
 }
 
 
+// array getGameList()
+NymphMessage* getGameList(int session, NymphMessage* msg, void* data) {
+	NymphMessage* returnMsg = msg->getReplyMessage();
+	
+	// Copy values from the game systems array into the new array.
+	std::vector<NymphType*>* tArr = new std::vector<NymphType*>();
+	for (uint32_t i = 0; i < mediaFiles.size(); ++i) {
+		std::map<std::string, NymphPair>* pairs = new std::map<std::string, NymphPair>;
+		
+		// 
+		
+		NymphPair pair;
+		std::string* key = new std::string("id");
+		pair.key = new NymphType(key, true);
+		pair.value = new NymphType(i);
+		pairs->insert(std::pair<std::string, NymphPair>(*key, pair));
+	
+		key = new std::string("section");
+		pair.key = new NymphType(key, true);
+		pair.value = new NymphType(&mediaFiles[i].section);
+		pairs->insert(std::pair<std::string, NymphPair>(*key, pair));
+		
+		key = new std::string("filename");
+		pair.key = new NymphType(key, true);
+		pair.value = new NymphType(&mediaFiles[i].filename);
+		pairs->insert(std::pair<std::string, NymphPair>(*key, pair));
+		
+		key = new std::string("type");
+		pair.key = new NymphType(key, true);
+		pair.value = new NymphType(mediaFiles[i].type);
+		pairs->insert(std::pair<std::string, NymphPair>(*key, pair));
+		
+		tArr->push_back(new NymphType(pairs, true));
+	}
+	
+	returnMsg->setResultValue(new NymphType(tArr, true));
+	msg->discard();
+	return returnMsg;
+}
+
+
 // --- LOG FUNCTION ---
 void serverLogFunction(int level, std::string logStr) {
 	std::cout << level << " - " << logStr << std::endl;
@@ -383,6 +450,30 @@ void statusUpdateCallback(uint32_t handle, NymphPlaybackStatus status) {
 }
 
 
+// --- ON FILE ADDED ---
+void onFileAdded(const Poco::DirectoryWatcher::DirectoryEvent& addEvent) {
+	std::cout << "Added: " << addEvent.item.path();
+	
+	// TODO: Handle.
+}
+
+
+// --- ON FILE MODIFIED ---
+void onFileModified(const Poco::DirectoryWatcher::DirectoryEvent& changeEvent) {
+	std::cout << "Modified: " << changeEvent.item.path();
+	
+	// TODO: Handle.
+}
+
+
+// --- ON FILE REMOVED ---
+void onFileRemoved(const Poco::DirectoryWatcher::DirectoryEvent& removeEvent) {
+	std::cout << "Removed: " << removeEvent.item.path();
+	
+	// TODO: Handle.
+}
+
+
 int main(int argc, char** argv) {
 	// Parse the command line arguments.
 	Sarge sarge;
@@ -416,20 +507,133 @@ int main(int argc, char** argv) {
 	}
 	
 	// Read in the configuration.
-	// TODO: no exposed configuration options yet.
-	/* IniParser config;
-	if (!config.load(config_file)) {
-		std::cerr << "Unable to load configuration file: " << config_file << std::endl;
-		return 1;
-	} */
-	//is_full_screen = config.getValue<bool>("fullscreen", false);
+	bool nc_gamesync = false;
+	std::string gameFolder;
+	INIReader config(config_file);
+	if (config.ParseError() != 0) {
+		/*std::cerr << "Unable to load configuration file: " << config_file << std::endl;
+		return 1;*/
+		nc_gamesync = false;
+	}
+	else {
+		// Get 'games' section and read the path.
+		std::set<std::string> cfg_sections = config.Sections();
+		std::cout << "Found " << cfg_sections.size() << " sections in the config file." << std::endl;
+		nc_gamesync = config.GetBoolean("games", "enable", true);
+		gameFolder = config.Get("games", "path", "games");
+	}
+	
+	if (nc_gamesync) {
+		// Check that path is a valid directory.
+		fs::path gamedir = gameFolder;
+		if (!fs::is_directory(gamedir)) {
+			std::cout << "Path is not a valid directory: " << gameFolder << ". Skipping." << std::endl;
+		}
+		else {
+			// TODO: Scan the available systems in the 'games' folder.
+			// Use the 'system.ini' file in each sub-folder to compile a list of available systems
+			// and their games.
+			// Start by getting the list of sub-folders.
+			
+			
+			// Attempt to open the 'system.ini' file per sub-folder. If found, read in
+			// system details and scan the 'games' sub-sub-folder for game filenames to add.
+		
+			// First iterate through the directory to filter out the game folders.
+			for (fs::recursive_directory_iterator next(gamedir); next != fs::end(next); next++) {
+				fs::path sysc = next->path();
+				sysc /= "system.ini";
+				fs::path sysdir = next->path();
+				fs::path romdir = sysdir;
+				romdir /= "roms";
+				fs::path savedir = sysdir;
+				savedir /= "saves";
+				if (!fs::is_regular_file(sysc)) {
+					continue; 
+				}
+				
+				// Open the INI file and process it.
+				INIReader syscfg(sysc.string());
+				
+				GameSystem gs;
+				gs.name = syscfg.Get("", "name", "");
+				gs.long_name = syscfg.Get("", "long_name", "");
+				gs.extensions = syscfg.Get("", "extensions", "");
+				gs.launch_cmd = syscfg.Get("", "launch_cmd", "");
+				gs.theme = syscfg.Get("", "theme", "");
+				
+				if (gs.name.empty() || gs.long_name.empty() || gs.launch_cmd.empty() || gs.theme.empty()) {
+					// Issue with the INI file. Report and skip this folder.
+					std::cout << "Missing value in system.ini file. Skipping folder..." << std::endl;
+					continue;
+				}
+				
+				// Unpack extensions and scan for ROMs.
+				Poco::StringTokenizer tokens(sysc.string(), ",", Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM);
+				uint32_t tc = tokens.count();
+				if (tc < 1) {
+					std::cerr << "Zero system extensions found. Skipping system..." << std::endl;
+					continue;
+				}
+				
+				if (!fs::is_directory(romdir)) {
+					std::cout << "Path is not a valid directory: " << romdir << ". Skipping." << std::endl;
+					continue;
+				}
+				
+				// Iterate through the directory to filter out the media files.
+				for (fs::recursive_directory_iterator rd(romdir); rd != fs::end(rd); rd++) {
+					fs::path fe = rd->path();
+					//std::cout << "Checking path: " << fe.string() << std::endl;
+					if (!fs::is_regular_file(fe)) {
+						continue; 
+					}
+					
+					std::string ext = fe.extension().string();
+					ext.erase(0, 1);	// Remove leading '.' character.
+					
+					// Check we have this extension in the filter.
+					if (tokens.has(ext)) {
+						// Add to list.
+						Game g;
+						g.name = fe.filename().string();
+						gs.games.push_back(g);
+					}
+				}
+				
+				// Scan for any save files.
+				for (fs::recursive_directory_iterator dit(savedir); dit != fs::end(dit); dit++) {
+					fs::path fe = dit->path();
+					if (!fs::is_regular_file(fe)) {
+						continue;
+					}
+					
+					// TODO: need to filter save file extensions too?
+					// Save files with libretro are apparently all *.srm extensions.
+					std::string ext = fe.extension().string();
+					ext.erase(0, 1);	// Remove leading '.' character.
+					
+					// Check we have this extension in the filter.
+					//if (tokens.has(ext)) {
+					if (ext == "srm") {
+						// Add to list.
+						Save s;
+						s.name = fe.filename().string();
+						gs.saves.push_back(s);
+					}
+				}
+			
+				gameSystems.push_back(gs);
+			}
+		}
+	}
 	
 	// Obtain the list of directories to scan.
 	std::cout << "Scanning directories..." << std::endl;
 	INIReader folderList(folders_file);
 	if (folderList.ParseError() != 0) {
 		std::cerr << "Failed to parse the '" << folders_file << "' file." << std::endl;
-		return false;
+		return 1;
 	}
 	
 	std::set<std::string> sections = folderList.Sections();
@@ -478,6 +682,13 @@ int main(int argc, char** argv) {
 				mediaFiles.push_back(mf);
 			}
 		}
+	
+		// Register a DirectoryWatcher for the media folder.
+		Poco::DirectoryWatcher* dw = new Poco::DirectoryWatcher(dir.string());
+		dw->itemModified		+= Poco::delegate(&onFileModified);
+		dw->itemAdded		+= Poco::delegate(&onFileAdded);
+		dw->itemRemoved		+= Poco::delegate(&onFileRemoved);
+		dirwatchers.push_back(dw);
 	}
 	
 	// Initialise the server.
@@ -504,6 +715,14 @@ int main(int argc, char** argv) {
 	NymphMethod playMediaFunction("playMedia", parameters, NYMPH_UINT8, playMedia);
 	NymphRemoteClient::registerMethod("playMedia", playMediaFunction);
 	
+	// array getGameList()
+	NymphMethod getGameListFunction("getGameList", parameters, NYMPH_ARRAY, getGameList);
+	NymphRemoteClient::registerMethod("getGameList", getGameListFunction);
+	
+	// ?? addGame()
+	
+	// ?? updateSave()
+	
 	// Install signal handler to terminate the server.
 	signal(SIGINT, signal_handler);
 	
@@ -520,6 +739,11 @@ int main(int argc, char** argv) {
 	std::cout << "Starting NyanSD on port 4005 UDP..." << std::endl;
 	NyanSD::startListener(4005);
 	
+	// TODO: Announce the NCMS on the network if game synchronisation is enabled.
+	// TODO: Handle synchronisation with other NCMS instances if found.
+	// TODO: Perform regular polling of remote file/NCMS content changes, as needed.
+	
+	
 	// Wait for the condition to be signalled.
 	gMutex.lock();
 	gCon.wait(gMutex);
@@ -529,6 +753,10 @@ int main(int argc, char** argv) {
 	// Clean-up
 	NyanSD::stopListener();
 	NymphRemoteClient::shutdown();
+	
+	for (uint32_t i = 0; i < dirwatchers.size(); i++) {
+		delete dirwatchers[i];
+	}
 	
 	// Wait before exiting, giving threads time to exit.
 	Thread::sleep(2000); // 2 seconds.
